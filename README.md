79f884945ee40f504e3a100c5e8dab5d63dab8d6
magnus <magnus@massivemonster.org>
2015-10-11 21:05:29 +0200

    Added script and read med

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# avroflat
-Python utility script to take an apache avro avpr file and turn it into schemafiles with dependent schemas interweaved
+avroflat
+==============
+
+A Python utility script to convert an apache avro avpr file into several schemas and flatten them at the same time (including dependent schemas into one schema which often causes some headache with non standard apache avro library languages)
+
+Dependencies
+===============
+
+1. Tested with python 2.7*
+2. Apache Avro python libraries needs to be installed
+
+Running it
+================
+
+The script accepts 2 commandline parameters
+
+1. Path to input avpr file
+2. Path to outputfolder where schemas should be stored
+
+e.g. python avro_flat "TEST1.avpr" "~/resultschemas"
+
+
+
---
+++ avro_flat.py
@@ -0,0 +1,52 @@
+import avro.protocol as avpr
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+import os
+import json
+import logging
+import sys
+
+def main(avprFile,outputFolder):
+	if not os.path.exists(outputFolder):
+		os.makedirs(outputFolder)
+	schema_formats = avpr.parse(open(avprFile, "r").read()) 
+	sorted([(v.type, k) for k, v in schema_formats.types_dict.items()])
+	for k, v in schema_formats.types_dict.items():
+		logging.info('Processing key:'+str(k))
+		checkAndProcessSchema(k,v.to_json(),outputFolder)
+	
+
+def checkAndProcessSchema(schemaName,jsonData,outputDir):
+	#check if we have a protocol definition on the schema
+	#if "protocol" in jsonData:
+	logging.info('Found protocol schema:'+schemaName)
+	writeJsonToFile(schemaName,outputDir,jsonData,jsonData['namespace'])
+
+def writeJsonToFile(schemaName,outputDir,data,namespace):
+	logging.info("Writing schema with name:"+schemaName+" and namespace:"+namespace)
+	outputFolder=buildFolderNameForSchema(outputDir,namespace)
+	#check if it exists otherwise create it
+	if not os.path.exists(outputFolder):
+		os.makedirs(outputFolder)
+	outFileName=os.path.join(outputFolder,schemaName+".avsc")
+	outFile = open(outFileName, "w")
+	outFile.write(json.dumps(data, indent=4, sort_keys=True))
+	outFile.close()
+	logging.info('Wrote data to:'+outFileName)
+
+def buildFolderNameForSchema(outputDir,namespace):
+	#replace all dots with slashes in namespace
+	outputNameSpaceFolder=namespace.replace(".","/")
+	return os.path.join(outputDir,outputNameSpaceFolder)
+
+if __name__ == '__main__':
+	logging.basicConfig(level=logging.INFO)
+	total = len(sys.argv)
+	
+	if total==3:
+		avprFile=sys.argv[1]
+		outputFolder=sys.argv[2]
+		main(avprFile,outputFolder)
+	else:
+		
+		logging.error("Missing required input parameters: <input avpr file> <output folder>")
